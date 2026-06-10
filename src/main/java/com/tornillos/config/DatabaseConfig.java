f9029c5d833b4041e6abb02fdb6dcd84be12cc02
Christoper Robles <zS24016704@estudiantes.uv.mx>
2026-06-10 02:22:44 -0600

    .

--- src/main/java/com/tornillos/config/DatabaseConfig.java
+++ src/main/java/com/tornillos/config/DatabaseConfig.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.io.InputStream;
 import java.util.logging.Logger;
 
 public class DatabaseConfig {
